d0aa7c48579496f3ae2fa7f2221641071351a924
WHJR-G8 <84369631+WHJR-G8@users.noreply.github.com>
2021-10-20 17:11:18 +0530

    Update Sol_Pair.py

--- Sol_Pair.py
+++ Sol_Pair.py
@@ -15,7 +15,9 @@ def draw_rect(s1,s2): #Function used to draw rectangles
         turtle.right(90)
     
     turtle.end_fill() #End the color filling
-    
+
+#Student 1
+
 position(10,-10,"red") #Function call to start the color filling for the upper portion i.e. triangle and move the turtle to the position (10, -10)
 
 for i in range(3): #Draw the triangle
@@ -27,6 +29,8 @@ turtle.end_fill()
 position(85,-40,"blue") #Function call to start the color filling for the right side rectangle and move the turtle to the position (85, -40)
 
 draw_rect(40,150) #Function call to draw the right side rectangle
+
+#Student 2
 
 position(10,-10,"green") #Function call to start the color filling for the middle rectangle and move the turtle to the position (10, -10)
 
